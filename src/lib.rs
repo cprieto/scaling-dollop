01bdfdf97478dab906beffc4a317cc0810652553
Cristian Prieto <me@cprieto.com>
2026-02-13 13:46:25 +0100

    made errors public

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,27 @@
-mod errors;
+use std::io::{ErrorKind, Read, Result as IOResult};
+use byteorder::ReadBytesExt;
+
+pub mod errors;
 pub mod memo;
+
+fn read_until_terminator<R: Read>(reader: &mut R, delimiter: &[u8]) -> IOResult<Vec<u8>> {
+    assert!(!delimiter.is_empty(), "delimiter must not be empty");
+
+    let mut output = vec![];
+    loop {
+        match reader.read_u8() {
+            Ok(b) => {
+                output.push(b);
+                if output.len() >= delimiter.len() {
+                    let size = output.len() - delimiter.len();
+                    if output[size..] == *delimiter {
+                        output.truncate(size);
+                        return Ok(output);
+                    }
+                }
+            }
+            Err(err) if err.kind() == ErrorKind::UnexpectedEof => return Ok(output),
+            Err(err) => return Err(err),
+        }
+    }
+}
