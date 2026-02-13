a68ec212676e571d24131f275ed0a61433ff95f0
Cristian Prieto <me@cprieto.com>
2026-02-13 13:21:48 +0100

    More research into format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::fs::File;
 use std::io::{Cursor, Seek, SeekFrom};
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
 
 fn main() -> anyhow::Result<()> {
-    let mut file = File::open("BLAH.DBT")?;
-    let x = file.read_u32::<LittleEndian>()?;
+    let mut file = File::open("FOXMEMO.FPT")?;
+    let next = file.read_u32::<BigEndian>()?;
 
-    println!("pos: {x}");
-    file.seek(SeekFrom::Start(16))?;
-    let x = file.read_u8()?;
-    println!("pos: {x}");
+    println!("next memo: {next}");
+    file.seek(SeekFrom::Start(6))?;
+    let block_size = file.read_u16::<BigEndian>()?;
+    println!("block size: {block_size}");
+
+    // Move past to first byte of block
+    file.seek(SeekFrom::Start(512))?;
+    let x = file.read_u32::<BigEndian>()?;
+    println!("record type: {x:#04x}");
+
+    let next_record = block_size as u32 * next;
+    println!("next block would be at {next_record:#05x}");
 
     Ok(())
 }
