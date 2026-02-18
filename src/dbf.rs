66630fd5205c97aab2286ecfef0fa38bb136f8ed
Cristian Prieto <cristian.prieto@coralogix.com>
2026-02-18 15:53:32 +0100

    Work in header parsing for DBF

--- src/dbf.rs
+++ src/dbf.rs
@@ -1,6 +1,10 @@
-use std::fmt::{Display, Formatter};
 use crate::errors;
 use crate::errors::Error;
+use crate::errors::Error::FileFormat;
+use byteorder::ReadBytesExt;
+use std::fmt::{Display, Formatter};
+use std::io::{Read, Seek, SeekFrom};
+use time::{Date, Month};
 
 #[derive(Debug)]
 enum DbfVersion {
@@ -27,7 +31,7 @@ impl TryFrom<u8> for DbfVersion {
             0x03 => Ok(DbfVersion::Dbase),
             0x83 => Ok(DbfVersion::Dbase3WithMemo),
             0x8B => Ok(DbfVersion::Dbase4WithMemo),
-            _ => Err(Error::FileFormat(format!("unknown version: {value:#04x}")))
+            _ => Err(Error::FileFormat(format!("unknown version: {value:#04x}"))),
         }
     }
 }
@@ -35,4 +39,47 @@ impl TryFrom<u8> for DbfVersion {
 struct DbfHeader {
     version: DbfVersion,
     last_update: Date,
+    num_records: u32,
+    start: u16,
+    record_length: u16,
+}
+
+impl DbfHeader {
+    fn from_reader<T: Read + Seek>(reader: &mut T) -> Result<Self, Error> {
+        reader.seek(SeekFrom::Start(0))?;
+        let version = reader.read_u8()?;
+        let version = DbfVersion::try_from(version)?;
+        let mut last_update = Vec::with_capacity(6);
+        reader.take(6).read_to_end(&mut last_update)?;
+        let last_update = parse_dbf_date(&last_update)?;
+
+        todo!()
+    }
+}
+
+// TODO: this implementation is fishy, I have to check against files
+fn parse_dbf_date(s: &[u8]) -> Result<Date, Error> {
+    let date =
+        std::str::from_utf8(s).map_err(|_| FileFormat("invalid binary date in header".into()))?;
+
+    let year = &date[0..4];
+    let year = year
+        .parse::<i32>()
+        .map_err(|_| FileFormat(format!("invalid year for date {year}")))?;
+
+    let month = &date[4..6];
+    let month = month
+        .parse::<u8>()
+        .map_err(|_| FileFormat(format!("invalid month for date: {month}")))?;
+
+    let month = Month::try_from(month)
+        .map_err(|_| FileFormat(format!("invalid month for date: {month}")))?;
+
+    let day = &date[6..8];
+    let day: u8 = date
+        .parse()
+        .map_err(|_| FileFormat(format!("invalid day for date: {day}")))?;
+
+    Date::from_calendar_date(year, month, day)
+        .map_err(|err| FileFormat(format!("invalid date: {date}")))
 }
